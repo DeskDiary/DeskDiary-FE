df0ed5b5be9be73ad576eece288aec5475180803
Banana-Master <tjdgh0313@gmail.com>
2023-10-14 09:46:16 +0900

    💄 캠 방 디자인

--- src/components/room/RoomSideBar.tsx
+++ src/components/room/RoomSideBar.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../../images/logo.svg';
 
 type RoomSideBarProps = {};
 
 const RoomSideBar: React.FC<RoomSideBarProps> = () => {
-  return <Body>123</Body>;
+  return (
+    <Body>
+      <LogoImg src={logo} alt="" />
+      <TimeBox>시간기록 Box</TimeBox>
+    </Body>
+  );
 };
 
 const Body = styled.div`
-  background: #ababab;
+  background: var(--gray-09, #424242);
   width: 224px;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LogoImg = styled.img`
+  margin-top: 20px;
+  width: 224px;
+  height: 56px;
+  flex-shrink: 0;
+`;
+
+const TimeBox = styled.div`
+  margin-top: 178px;
+  width: 224px;
+  height: 468px;
+  flex-shrink: 0;
+  background: #ababab;
 `;
 export default RoomSideBar;
