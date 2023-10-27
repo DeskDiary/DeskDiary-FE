fd59306baac2a656c0c68f055c6e09b2876bb3b9
SsSseo <tjgus0156@kakao.com>
2023-10-27 21:32:34 +0900

    💄 채팅 리스트 길이 수정

--- src/pages/room/components/chat/ChatBox.tsx
+++ src/pages/room/components/chat/ChatBox.tsx
@@ -183,7 +183,7 @@ const ChatList = styled.div`
   gap: 15px;
 
   height: 100%;
-  width: calc(100% - 60px);
+  width: calc(100% - 100px);
   padding: 20px;
   border-bottom: 1px solid var(--gray-05);
   overflow: scroll;
