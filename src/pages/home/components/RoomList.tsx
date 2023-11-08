9d515dbf8df25e8d5e8f06162cf8d527e42448e5
김성호 <tjdgh0313@gmail.com>
2023-11-08 19:37:58 +0900

    🐛  메인 조회 복구

--- src/pages/home/components/RoomList.tsx
+++ src/pages/home/components/RoomList.tsx
@@ -13,8 +13,6 @@ import {
   fetchUser,
 } from '../../../axios/api';
 import RoomCard from './RoomCard';
-import { useRecoilState } from 'recoil';
-import axios from 'axios';
 import { act } from '@testing-library/react';
 
 type RoomListProps = {
@@ -36,26 +34,7 @@ const fetchFunctions = {
 const RoomList: React.FC<RoomListProps> = ({ label, show }) => {
   const [isPopular, setIsPopular] = useState(true);
   const [sort, setSort] = useState('Popular');
-  const target = useRef<HTMLElement | null>(null!);
-
   const [roomList, setRoomList] = useState<room[]>([]);
-
-  const [count, setCount] = useState(roomList.length);
-
-  const options = {
-    threshold: 1.0,
-  };
-
-  const callback = () => {
-    queryClient.invalidateQueries(fetchName);
-  };
-
-  const observer = new IntersectionObserver(callback, options);
-
-  useEffect(() => {
-    observer.observe(target.current!);
-  }, []);
-
   const changePopular = (value: boolean) => {
     setIsPopular(value);
     if (value) {
@@ -67,40 +46,33 @@ const RoomList: React.FC<RoomListProps> = ({ label, show }) => {
 
   let fetchName = show + sort;
 
-  console.log('카운트1', count);
-  console.log(roomList.length);
-  const queryClient = useQueryClient();
+
   const { data } = useQuery<room[], Error>(
-    fetchName, // 쿼리 키를 배열로 만들어 fetchName, show, sort 추가
+    [fetchName, show, sort], // 쿼리 키를 배열로 만들어 fetchName, show, sort 추가
     async () => {
       const fetchFunc =
         fetchFunctions[fetchName as keyof typeof fetchFunctions];
       if (fetchFunc) {
-        console.log('카운트2', count);
-        console.log(count > 0 ? roomList[count - 1].roomId : 0);
-        const result: room[] = await fetchFunc(
-          count > 0 ? roomList[count - 1].roomId : 0,
-        );
-        return result;
+        const result = await fetchFunc(0);
+        return result.QueryResults || result;
       } else {
         throw new Error('Invalid fetchName');
       }
     },
-    {
-      
-    },
+    {},
   );
   useEffect(() => {
-    console.log('데이터', data);
-    // Set을 Array로 변환하고 중복을 제거한 후 다시 Set으로 변환
-    const uniqueRoomSet = new Set([...roomList, ...(data || [])]);
-    setRoomList(Array.from(uniqueRoomSet));
+    if(fetchName === 'fetchRoomPopular') {
+      setRoomList(data || []);
+    } else if(fetchName === 'fetchRoomLatest') {
+      setRoomList(data || []);
+    } else if(fetchName === 'fetchRoomTopPopular') {
+      setRoomList(data || []);
+    } else if(fetchName === 'fetchRoomTopLatest') {
+      setRoomList(data || []);
+    }
+    
   }, [data]);
-
-  useEffect(() => {
-    console.log('****');
-    setCount(roomList.length);
-  }, [roomList]);
 
   useEffect(() => {
     if (isPopular) {
@@ -137,7 +109,6 @@ const RoomList: React.FC<RoomListProps> = ({ label, show }) => {
           return <RoomCard key={room.uuid} room={room} fetch={fetchName} />;
         })}
       </JoinedRooms>
-      <Target ref={target} />
     </List>
   );
 };
@@ -226,10 +197,5 @@ const JoinedRooms = styled.div`
   }
 `;
 
-const Target = styled.div<{ ref: any }>`
-  width: 50vw;
-  height: 100px;
-  border: 1px solid tomato;
-`;
 
 export default RoomList;
