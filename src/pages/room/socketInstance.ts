07c5448af4cd64c23ee8e9a5396aa6cbbf20d2ac
SsSseo <tjgus0156@kakao.com>
2023-11-14 16:27:30 +0900

    ✅ 소켓 테스트

--- src/pages/room/socketInstance.ts
+++ src/pages/room/socketInstance.ts
@@ -5,6 +5,6 @@ const socket = io(`${process.env.REACT_APP_SERVER_URL!}`, {
   reconnection: true, // 재연결 시도 활성화
   reconnectionAttempts: 10,
   reconnectionDelay: 3000,
-  transports: ['websocket']
+  // transports: ['websocket']
 });
 export default socket;
